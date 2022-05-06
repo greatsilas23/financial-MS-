a1de2a75254a08751469b38f992151c22959b502
greatsilas23 <nsilasn@gmail.com>
2022-05-06 14:47:21 +0300

    first commit

--- Desktop/BlairTAgristores/App.js
+++ Desktop/BlairTAgristores/App.js
@@ -1,51 +1,74 @@
-import React, { Component } from "react"
-import Styles from "./Styles.js"
+import React, {Component} from 'react'
+import { View, Image, TouchableOpacity } from 'react-native'
+import menu from './res/menu.png'
+import cart from './res/cart.jpg'
+import logIn from './res/logIn.jpeg'
+import Icon from './res/Icon.js'
+import Tablink from './res/Tablink.js'
 
-class App {
+export default class AgristoresApp extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			chat: {
-				toggle: "",
-				deets: "",	
-				profile: "",
-				find: "",
-				icons: "",
-				action: ""
-			}
-			view: {
-				icons: "",
-				profile: "",
-				action: "",
-				deets: {
-					meeting:"",
-					landlines: ""
-				},
-				toggle: ""			
-			}
+			loginTabVisible: true
 		}
+
 	}
-	const styles = Styles
 	render() {
 		return(
-			<ChatComponent 
-				profileChat = {this.state.chat.profile}
-				findChat = {this.state.chat.find}
-				iconChat = {this.state.chat.icons}
-				actionChat = {this.state.chat.action}
-				toggleChat = {this.state.chat.toggle}
-				deetsChat = {this.state.chat.deets}
-			/>
-			<ViewComponent 
-				profileMesage = {this.state.view.profile}
-				iconMessage = {this.state.view.icon}
-				actionMessage = {this.state.view.action}
-				toggleMessage = {this.state.view.toggle}
-				deetsMessage = {this.state.view.deets}
-			/>	
+			<View style={styles.main.container}>
+
+
+				{this.state.loginTabVisible &&
+					<View style={styles.login.container}>
+						<View style={styles.login.content}>						
+							<TextInput
+							style={styles.login.input}
+	placeholder="ID002"
+							/>
+							<TextInput
+	style={styles.login.input}							placeholder="******"
+							/>
+							<Button
+style={styles.login.button}							onPress={this.handleLoginDetails}
+title="LOGIN"
+color="#2e856e"
+							</Button>
+						</View>
+					</View>
+				}
+			</View>
 		)
-	}
+	
 }
 
-export default App
-	 
+const styles = Stylesheet.create({
+	main: {
+		container: {
+			width: '100vw',
+			height: '100vh',
+			backgroundColor: '#006a4e'
+		}
+	},	
+	login : {
+		container: {
+	
+			height: '50vh',
+			width: '50vw',
+			backgroundColor: '#2e856e'
+		},
+		content: {
+			height: '35vh',
+			width: '35vw',
+			backgroundColor: '#b8d5cd'
+		},
+		input: {
+			height: '7vh',
+			width: '30vw',
+			backgroundColor: '#b8d5cd',
+			borderRadius: '3vw'
+		}
+	}
+	
+})
+
