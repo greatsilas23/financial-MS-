891f4c3ae2d609da813b2f6f02e044dad675efba
greatsilas23 <nsilasn@gmail.com>
2022-05-06 20:23:14 +0300

    first commit

--- Desktop/Clones/project-app/src/App.js
+++ Desktop/Clones/project-app/src/App.js
@@ -7,6 +7,7 @@ import MenuBar from './components/MenuBar.js'
 import ContactUs from './components/ContactUs.js'
 import Navigation from './components/Navigation.js'
 import Copyright from './components/Copyright.js'
+import cto from './resources/cto.jpg'
 class App extends React.Component {
   constructor(props){
     super(props)
@@ -50,24 +51,32 @@ class App extends React.Component {
                     <img src={resources.basic_info}/>
                 </form>
                 </div>
+                <div style={{width: '130px', height: '130px', padding: '5px',backgroundColor: '#fff', borderRadius: '10px'}}>
+                    <img src={cto} width="127.5" height="127.5"/>
+                </div>
+                <div>
+                    <p>Username: example123</p>
+                    <p>User password: ****</p>
+                    <p>User role</p>
+                </div>
                 <div>
                     <table style={{border: 'solid 2px #000'}}>
-                        <tr>
+                        <tr style={{border: 'solid 2px #000'}}>
                             <td>Username</td>
                             <td>User password</td>
                             <td>User role</td>
                         </tr>
-                        <tr>
+                        <tr style={{border: 'solid 2px #000'}}>
                             <td> example123 </td>
                             <td> 1234 </td>
                             <td> admin </td>
                         </tr>
-                        <tr>
+                        <tr style={{border: 'solid 2px #000'}}>
                             <td> example1234 </td>
                             <td> 1111 </td>
                             <td> admin </td>
                         </tr>
-                        <tr>
+                        <tr style={{border: 'solid 2px #000'}}>
                             <td> example12345 </td>
                             <td> 0000 </td>
                             <td> admin </td>
